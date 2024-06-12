8042768cf1af47b3e6dd7650a8a8f431818c57f8
sungkyunpark1992 <117180248+sungkyunpark1992@users.noreply.github.com>
2024-06-12 17:39:54 +0900

    save

--- 0612_study/react-basic/src/Array/CreateUser.js
+++ 0612_study/react-basic/src/Array/CreateUser.js
@@ -1,23 +1,24 @@
 import React from "react";
 
 // input태그 2개(계정,이메일),버튼(추가 처리... )
-function CreateUser({username, email, onChange, onCreate}) {
-  return(
-    <div>
-      <input 
-        name="username"
-        placeholder="계정명"
+function CreateUser({ username, email, onChange, onCreate }) {
+  return (
+    <form>
+      <input
+        name={"username"}
+        placeholder={"계정명"}
         onChange={onChange}
         value={username}
       />
       <input
+        type={"email"}
         name="email"
         placeholder="이메일"
         onChange={onChange}
         value={email}
       />
       <button onClick={onCreate}>등록</button>
-    </div>
+    </form>
   );
 }
 
